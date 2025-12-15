03e71f5e8041d6f6cbafafb904b4f164376f363a
kawsar12759 <kawsar15-12759@diu.edu.bd>
2025-12-15 23:47:32 +0600

    Created Project and Added Navbar

---
+++ components/NavBar.tsx
@@ -0,0 +1,46 @@
+"use client"
+import { Button } from '@/components/ui/button';
+import { BookKey, BookOpen, Compass, Library, Plus } from 'lucide-react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react';
+
+const NavBar = () => {
+    const pathname = usePathname();
+    function isActive(path: string) {
+        if (pathname === path) {
+            return true;
+        }
+        return false;
+    }
+    return (
+        <nav className='border-b border-[#DAD3C8] bg-[#FCFAF7]/50 backdrop-blur-xs sticky top-0 z-50 px-4'>
+
+            <div className='mx-auto'>
+                <div className='flex items-center justify-between h-16'>
+                    <Link href="/" className='flex items-center gap-2'>
+                        <BookKey className='w-6 h-6' />
+                        <span className='text-xl font-bold text-foreground'>Shelfx</span></Link>
+                    <div className='flex items-center gap-2'>
+                        <Button variant={isActive("/") ? "default" : "ghost"} size="sm" asChild>
+                        <Link href="/" className='gap-2'><BookOpen className='w-4 h-4' />
+                                <span className='hidden sm:inline'>Feed</span></Link></Button>
+                        <Button variant={isActive("/explore") ? "default" : "ghost"} size="sm" asChild
+                        ><Link href="/explore" className='gap-2'><Compass className='w-4 h-4' />
+                                <span className='hidden sm:inline'>Explore</span></Link></Button>
+                        <Button variant={isActive("/add-book") ? "default" : "ghost"} size="sm" asChild
+                        ><Link href="/add-book" className='gap-2'><Plus className='w-4 h-4' />
+                                <span className='hidden sm:inline'>Add Book</span></Link></Button>
+                        <Button variant={isActive("/library") ? "default" : "ghost"} size="sm" asChild
+                        ><Link href="/library" className='gap-2'><Library className='w-4 h-4' />
+                                <span className='hidden sm:inline'>Library</span></Link></Button>
+                        <Button variant={"outline"}
+                        ><Link href="/">Sign In</Link></Button>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
